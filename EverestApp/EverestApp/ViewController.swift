bf9348742286b97575ce59ad44be4250b8213294
Jose Gamboa <jose.gamboa@avantica.net>
2019-02-12 23:28:15 -0600

    first commit

---
+++ EverestApp/EverestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EverestApp
+//
+//  Created by Jose Gamboa on 2/12/19.
+//  Copyright © 2019 Jose Gamboa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
